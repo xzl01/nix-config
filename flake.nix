c9904b3240f13e0c18fbfe054bc9400a51eba6ab
HarryLoong <harryloong8@outlook.com>
2025-09-16 21:45:12 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,38 +1,46 @@
 {
-  description = "A simple NixOS flake";
-
+  description = "xzl的nix配置 with hydenix";
+  
   inputs = {
-    # NixOS 官方软件源，这里使用 nixos-25.05 分支
+    # 基础系统依赖
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    
+    # Hydenix扩展
     hydenix = {
-      # Available inputs:
-      # Main: github:richen604/hydenix
-      # Dev: github:richen604/hydenix/dev
-      # Commit: github:richen604/hydenix/<commit-hash>
-      # Version: github:richen604/hydenix/v1.0.0
       url = "github:richen604/hydenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    
+    # 系统工具依赖
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
- 
 
-  outputs =
-    { ... }@inputs:
-    let
-      hydenixConfig = inputs.hydenix.inputs.hydenix-nixpkgs.lib.nixosSystem {
-        inherit (inputs.hydenix.lib) system;
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./configuration.nix
-	  ./base/fonts.nix
-        ];
-      };
-
-    in
-    {
-      nixosConfigurations.hydenix = hydenixConfig;
+  outputs = { self, nixpkgs, hydenix, nix-index-database, ... }@inputs: {
+    # 基础系统配置
+    nixosConfigurations.nixos-xzl = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # 基础配置
+        ./configuration.nix
+        
+        # Hydenix核心模块
+        inputs.hydenix.inputs.hydenix-nixpkgs.lib.modules.common
+        
+        # 系统工具配置
+        {
+          nixpkgs.overlays = [ inputs.nix-index-database.overlays.default ];
+        }
+      ];
     };
-}
 
+    # 增强开发配置
+    nixosConfigurations.developer = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # 用户基础配置
+        ./configuration
